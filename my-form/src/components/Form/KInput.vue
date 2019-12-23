781f7080e6a6721be27b3c4cd02dd2cfb363c633
sanyuan <sanyuan.jw@foxmail.com>
2019-12-23 15:00:24 +0800

    add KInput

---
+++ my-form/src/components/Form/KInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="onInput" />
+  </div>
+</template>
+
+<script>
+/**
+ * 职能
+ * 1.双向绑定
+ * 2.触发校验
+ */
+export default {
+  name: 'KInput',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    onInput(e) {
+      // v-model 监听 input
+      this.$emit('input', e.taiget.value)
+      // 触发校验 -> 父级自己派发 自己监听
+      this.$parent.$emit('validate')
+    }
+  }
+}
+</script>
