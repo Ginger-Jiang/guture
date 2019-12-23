dcff80a2c6eef675e618d800be80c2daf7f80bed
Ginger <346600397@qq.com>
2019-12-23 12:49:54 +0800

    add v-model sugar

---
+++ my-form/src/components/Vmodel/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- vmodel 语法糖本质 -->
+    <input type="text" :value="value" @input="value = $event.target.value" />
+    <p>绑定的值是{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
